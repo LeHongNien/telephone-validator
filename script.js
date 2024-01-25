404249f16decb9e4415a2036c560ed1c356e767e
Le Hong Nien <lehongnien1@gmail.com>
2024-01-25 12:09:14 +0800

    final commit

--- script.js
+++ script.js
@@ -1,40 +1,21 @@
 const checkBtn = document.getElementById("check-btn"),
 clearBtn = document.getElementById("clear-btn"),
-textInput = document.getElementById("user-input"),
+userInput = document.getElementById("user-input"),
 resultsDiv = document.getElementById("results-div");
-
-
-checkBtn.addEventListener('click', validateNumber);
-clearBtn.addEventListener('click', clearDiv);
-
-function validateNumber(num) {
-    let userInput = document.getElementById("user-input").value;
-    if (userInput === "") {
-        alert('Please provide a phone number');
-    }
-    let regex = []
-    regex[0] = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/ /* 012-345-6789 */
-    regex[1] = /^\([0-9]{3}\)[0-9]{3}-[0-9]{4}$/ /* (012)345-6789 */
-    regex[2] = /^\([0-9]{3}\)\s[0-9]{3}-[0-9]{4}$/ /* (012) 345-6789 */
-    regex[3] = /^[0-9]{3}\s[0-9]{3}\s[0-9]{4}$/ /* 012 345 6789 */
-    regex[4] = /^[0-9]{10}$/ /* 0123456789 */
-    regex[5] = /^1\s[0-9]{3}\s[0-9]{3}\s[0-9]{4}$/ /* 1 012 345 6789 */
-    regex[6] = /^1\s[0-9]{3}-[0-9]{3}-[0-9]{4}$/ /* 1 012-345-6789 */
-    regex[7] = /^1\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{4}$/ /* 1 (012) 345 6789 */
-    regex[8] = /^1\([0-9]{3}\)\s[0-9]{3}-[0-9]{4}$/ /* 1 (012) 345-6789 */
   
-    for (let i in regex) {
-      // Return true if regex is matched
-      let isValid = regex[i].test(num)
-  
-      if (isValid) {
-        return resultsDiv.innerHTML = `Valid US number: ${userInput}`;
-      } else {
-        resultsDiv.innerHTML = `Invalid US number: ${userInput}`;
-      }
+checkBtn.addEventListener('click', () => {
+    if (userInput.value === '') {
+        alert('Please provide a phone number.');
+    } else {
+        resultsDiv.innerHTML = `${telephoneCheck(userInput.value) ? 'Valid' : 'Invalid'} US number: ${userInput.value}`
     }
-};
+})
+  
+clearBtn.addEventListener('click', () => {
+    userInput.value = '';
+    resultsDiv.innerHTML = '';
+})
 
-function clearDiv() {
-    resultsDiv.innerHTML = "";
-};
+function telephoneCheck(num) {
+    return /^(1\s?)?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/.test(num);
+}
